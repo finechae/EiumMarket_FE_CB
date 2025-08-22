753486f00d668a3b997d0e35dd933fbd01565256
jeongchaehwan <wjdcoghks1428@naver.com>
2025-08-22 21:53:39 +0900

    fix: Vite 프록시 설정으로 CORS 오류 해결

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,16 @@ export default defineConfig({
     react(), 
     tailwindcss()
   ],
-}) 
+
+  server: {
+    proxy: {
+      // '/api'로 시작하는 모든 요청을 target 주소로 보냅니다.
+      '/api': {
+        target: 'http://3.3.4.95.220', // 백엔드 서버 주소
+        changeOrigin: true, // cross-origin 요청을 위해 필요한 설정
+        // 요청 주소에서 '/api' 부분을 제거하고 보냅니다.
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
+})
